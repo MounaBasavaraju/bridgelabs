6f89139af00d7c8933d4c579cd798b9a2d304f3a
MounaBasavaraju <148189179+MounaBasavaraju@users.noreply.github.com>
2024-01-25 13:22:09 +0530

    Add files via upload

---
+++ bridgelabs/addition.java
@@ -0,0 +1,12 @@
+public class addition {
+    public static void main(String args[])
+    {
+        int a=12,b=10,c;
+        c=a+b;
+        System.out.println("addition of two numbers is" +c);
+        c=a-b;
+        System.out.println("subtraction of two numbers is " +c);
+
+
+    }
+}
